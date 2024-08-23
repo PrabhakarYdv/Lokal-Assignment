4f4c30a55d4f5326c303a4efa23254cbda0bcecb
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-08-23 10:44:08 +0530

    Output Added for ReadeMe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
-
     kapt("androidx.room:room-compiler:2.6.0")
 
     // Coroutines
